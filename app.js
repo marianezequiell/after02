645ee5f336a06ff5b9deb0efb06455e003d54ea5
marianoEzequiell <mariano_ef@live.com>
2021-11-23 21:16:22 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT || 3000)
+
+app.get('/', (req, res) => {
+    console.log("GET request");
+
+    res.send("Saludos")
+})
